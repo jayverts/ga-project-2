67bfc967ead4982dc6179afa20fe82b4f670062b
jayverts <j.verdolino@gmail.com>
2017-08-14 17:46:55 -0600

    trying to get the sites to stay

--- models/index.js
+++ models/index.js
@@ -1,9 +1,9 @@
 var mongoose = require("mongoose");
-mongoose.connect(
-	process.env.MONGODB_URI ||	
-	process.env.MONGOLAB_URI ||
-	process.env.MONGOHQ_URL ||
-	"mongodb://localhost/project-2");
+// mongoose.connect(
+// 	process.env.MONGODB_URI ||	
+// 	process.env.MONGOLAB_URI ||
+// 	process.env.MONGOHQ_URL ||
+// 	"mongodb://localhost/project-2");
 
 var User= require("./user");
 var CampSite = require("./campSite");
